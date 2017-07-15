3402aa3ea9cb2153c14938d07c73c07da2d02434
Aadesh Randeria <ar.adaptivealgo@gmail.com>
2017-07-15 16:49:13 -0400

    mean blog: add update delete

--- public/lectures/mean-blog/app.js
+++ public/lectures/mean-blog/app.js
@@ -1,13 +1,45 @@
 angular
     .module('BlogApp', [])
-    .controller('BlogController', BlogController);
+    .controller('BlogController', blogController);
 
-function BlogController($scope) {
+function blogController($scope) {
     $scope.blogPosts = [
-        {title: 'Post 1', content: 'Content 1'},
-        {title: 'Post 2', content: 'Content 2'},
-        {title: 'Post 3', content: 'Content 3'},
-        {title: 'Post 4', content: 'Content 4'},
-        {title: 'Post 5', content: 'Content 5'}
+        {title: 'Post 1', content: 'Content 1'}
     ];
+
+    $scope.addPost = addPost;
+    $scope.deletePost = deletePost;
+    $scope.deleleAll = deleleAll;
+    $scope.selectPost = selectPost;
+    $scope.updatePost = updatePost;
+    $scope.blog = {};
+
+    function addPost(blog) {
+        var newPost = {
+            title: blog.title,
+            content: blog.content
+        }
+        $scope.blogPosts.push(newPost);
+        blog.title = "";
+        blog.content = "";
+    }
+
+    function deletePost(index) {
+        $scope.blogPosts.splice(index, 1);
+    }
+
+    function deleleAll() {
+        $scope.blogPosts = [];
+    }
+
+    function selectPost(blog, index) {
+        $scope.selectedBlog = index;
+        $scope.blog.title = blog.title;
+        $scope.blog.content = blog.content;
+    }
+
+    function updatePost(blog) {
+        $scope.blogPosts[$scope.selectedBlog] = blog;
+        $scope.blog = {};
+    }
 }
