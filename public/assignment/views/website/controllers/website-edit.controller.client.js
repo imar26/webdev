001809039e4148905da0bc564818b2d545cc1625
Aadesh Randeria <ar.adaptivealgo@gmail.com>
2017-07-27 00:30:47 -0400

    assisngment 3 till page 7

---
+++ public/assignment/views/website/controllers/website-edit.controller.client.js
@@ -0,0 +1,9 @@
+(function () {
+    angular
+        .module("WebAppMaker")
+        .controller("EditWebsiteController", EditWebsiteController);
+
+    function EditWebsiteController() {
+        var vm = this;
+    }
+})();
